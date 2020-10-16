54d4d5a5be978e1c53935d8b6837051379872fe9
Jerome Franco <jerfranco@deloitte.com>
2020-10-16 20:04:42 +0800

    Complete MotionLayout animation

--- app/src/main/java/com/example/musicplaylist/MainActivity.kt
+++ app/src/main/java/com/example/musicplaylist/MainActivity.kt
@@ -1,37 +1,38 @@
 package com.example.musicplaylist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.TransitionAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val items = (1..20).map { "item $it" }
+    private val items = (1..20).map { "Song $it" }
     private val adapter = SimpleAdapter(items)
-    var rebuilt = false
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         songList.adapter = adapter
-        songList.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 
         root.setTransitionListener(object : TransitionAdapter() {
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                if (currentId == R.id.middle) {
-                    root.postDelayed({
-                        root.setTransition(R.id.middleToEnd)
-                    }, 500)
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+                if (endId == R.id.start) {
+                    window.statusBarColor = resources.getColor(R.color.neutral_000)
                 }
             }
 
-
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                if (currentId == R.id.middle && motionLayout?.progress == 1f) {
+                    window.statusBarColor = resources.getColor(R.color.yellow)
+                    root.setTransition(R.id.middleToEnd)
+                }
+            }
         })
     }
 }
