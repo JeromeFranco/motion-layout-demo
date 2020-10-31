ecc59109edda05b28117b890ae35a4a59db1a99f
Jerome Franco <jerfranco@deloitte.com>
2020-10-31 22:52:54 +0800

    initial setup

--- app/src/main/java/com/example/musicplaylist/MainActivity.kt
+++ app/src/main/java/com/example/musicplaylist/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.musicplaylist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.constraintlayout.motion.widget.TransitionAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,30 +13,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         songList.adapter = adapter
-
-        backButton.setOnClickListener {
-            root.setTransition(R.id.startToMiddle)
-            root.transitionToStart()
-        }
-
-        root.setTransitionListener(object : TransitionAdapter() {
-            override fun onTransitionStarted(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int
-            ) {
-                if (startId == R.id.start) {
-                    window.statusBarColor = resources.getColor(R.color.neutral_000)
-                }
-            }
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                if (currentId == R.id.middle) {
-                    window.statusBarColor = resources.getColor(R.color.yellow)
-                    root.postDelayed({
-                        root.setTransition(R.id.middleToEnd)
-                    }, 500)
-                }
-            }
-        })
     }
 }
